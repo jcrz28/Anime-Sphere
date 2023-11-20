4a7b18fb31e904252e5017115d64151873306bf0
John Leonard Cruz <cruzjohnleonard@gmail.com>
2023-11-20 17:25:45 -0600

    Fixed theme-toggle import

--- src/components/theme-toggle-switch.js
+++ src/components/theme-toggle-switch.js
@@ -1,10 +1,8 @@
-import {
-	Brightness4 as Brightness4Icon,
-	Brightness7 as Brightness7Icon,
-} from "@mui/icons-material";
 import React, { useContext } from "react";
 
-import { IconButton } from "@mui/material";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import IconButton from "@mui/material/IconButton";
 import { ThemeContext } from "../context/theme-context";
 import { useTheme } from "@mui/material/styles";
 
