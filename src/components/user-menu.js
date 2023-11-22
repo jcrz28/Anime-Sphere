962579e117bd9bf3484d2daa1639a5b50405dece
John Leonard Cruz <cruzjohnleonard@gmail.com>
2023-11-21 21:14:42 -0600

    Implemented mobile menu and regular menu

--- src/components/user-menu.js
+++ src/components/user-menu.js
@@ -1,7 +1,11 @@
+import * as React from "react";
+
 import AuthContext from "../context/auth-context";
 import Avatar from "@mui/material/Avatar";
 import Box from "@mui/material/Box";
-import { Home } from "@mui/icons-material";
+import Chip from "@mui/material/Chip";
+import Divider from "@mui/material/Divider";
+import Home from "@mui/icons-material/Home";
 import IconButton from "@mui/material/IconButton";
 import LibraryBooksIcon from "@mui/icons-material/LibraryBooks";
 import ListItemIcon from "@mui/material/ListItemIcon";
@@ -9,7 +13,7 @@ import Login from "@mui/icons-material/Login";
 import Logout from "@mui/icons-material/Logout";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
-import React from "react";
+import MoreIcon from "@mui/icons-material/MoreVert";
 import Tooltip from "@mui/material/Tooltip";
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
@@ -17,15 +21,28 @@ import { useNavigate } from "react-router-dom";
 const UserMenu = () => {
 	const authCtx = useContext(AuthContext);
 	const navigate = useNavigate();
-	const [menuAnchorEl, setMenuAnchorEl] = React.useState(null);
 
-	const isMenuOpen = Boolean(menuAnchorEl);
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
 
-	const handleMenuOpen = (event) => {
-		setMenuAnchorEl(event.currentTarget);
+	const isMenuOpen = Boolean(anchorEl);
+	const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+	const handleProfileMenuOpen = (event) => {
+		setAnchorEl(event.currentTarget);
 	};
+
+	const handleMobileMenuClose = () => {
+		setMobileMoreAnchorEl(null);
+	};
+
 	const handleMenuClose = () => {
-		setMenuAnchorEl(null);
+		setAnchorEl(null);
+		handleMobileMenuClose();
+	};
+
+	const handleMobileMenuOpen = (event) => {
+		setMobileMoreAnchorEl(event.currentTarget);
 	};
 
 	const handleLogout = () => {
@@ -49,90 +66,134 @@ const UserMenu = () => {
 		handleMenuClose();
 	};
 
-	return (
-		<React.Fragment>
-			<Tooltip title="Account settings">
-				<IconButton
-					onClick={handleMenuOpen}
-					size="small"
-					aria-controls={isMenuOpen ? "account-menu" : undefined}
-					aria-haspopup="true"
-				>
-					<Avatar sx={{ width: 32, height: 32 }} />
-				</IconButton>
-			</Tooltip>
-			<Menu
-				anchorEl={menuAnchorEl}
-				id="account-menu"
-				open={isMenuOpen}
-				onClose={handleMenuClose}
-				slotProps={{
-					elevation: 0,
-					sx: {
-						overflow: "visible",
-						filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-						mt: 1.5,
-						"& .MuiAvatar-root": {
-							width: 32,
-							height: 32,
-							ml: -0.5,
-							mr: 1,
-						},
-						"&:before": {
-							content: '""',
-							display: "block",
-							position: "absolute",
-							top: 0,
-							right: 14,
-							width: 10,
-							height: 10,
-							bgcolor: "background.paper",
-							transform: "translateY(-50%) rotate(45deg)",
-							zIndex: 0,
-						},
-					},
-				}}
-				anchorOrigin={{
-					vertical: "bottom",
-					horizontal: "right",
-				}}
-				transformOrigin={{
-					vertical: "top",
-					horizontal: "right",
-				}}
-				getContentAnchorEl={null}
-			>
-				<MenuItem onClick={handleHomePage}>
-					<ListItemIcon>
-						<Home fontSize="small" />
-					</ListItemIcon>
-					Home
+	const menuId = "primary-search-account-menu";
+	const renderMenu = (
+		<Menu
+			anchorEl={anchorEl}
+			anchorOrigin={{
+				vertical: "top",
+				horizontal: "right",
+			}}
+			id={menuId}
+			keepMounted
+			transformOrigin={{
+				vertical: "top",
+				horizontal: "right",
+			}}
+			open={isMenuOpen}
+			onClose={handleMenuClose}
+		>
+			{authCtx.isLoggedIn ? (
+				<MenuItem onClick={handleLogout}>
+					<Logout /> Log Out
 				</MenuItem>
-				{authCtx.isLoggedIn ? (
-					<Box>
-						<MenuItem onClick={handleLibraryPage}>
-							<ListItemIcon>
-								<LibraryBooksIcon fontSize="small" />
-							</ListItemIcon>
-							Library
-						</MenuItem>
-						<MenuItem onClick={handleLogout}>
-							<ListItemIcon>
-								<Logout fontSize="small" />
-							</ListItemIcon>
-							Logout
-						</MenuItem>
-					</Box>
-				) : (
-					<MenuItem onClick={handleSignIn}>
+			) : (
+				<MenuItem onClick={handleSignIn}>
+					<Login /> Sign In
+				</MenuItem>
+			)}
+		</Menu>
+	);
+
+	const mobileMenuId = "primary-search-account-menu-mobile";
+	const renderMobileMenu = (
+		<Menu
+			anchorEl={mobileMoreAnchorEl}
+			anchorOrigin={{
+				vertical: "top",
+				horizontal: "right",
+			}}
+			id={mobileMenuId}
+			keepMounted
+			transformOrigin={{
+				vertical: "top",
+				horizontal: "right",
+			}}
+			open={isMobileMenuOpen}
+			onClose={handleMobileMenuClose}
+		>
+			<MenuItem onClick={handleHomePage}>
+				<ListItemIcon>
+					<Home fontSize="small" />
+				</ListItemIcon>
+				Home
+			</MenuItem>
+			{authCtx.isLoggedIn ? (
+				<Box>
+					<MenuItem onClick={handleLibraryPage}>
 						<ListItemIcon>
-							<Login fontSize="small" />
+							<LibraryBooksIcon fontSize="small" />
 						</ListItemIcon>
-						Sign In
+						Library
 					</MenuItem>
+					<Divider />
+					<MenuItem onClick={handleLogout}>
+						<ListItemIcon>
+							<Logout fontSize="small" />
+						</ListItemIcon>
+						Logout
+					</MenuItem>
+				</Box>
+			) : (
+				<MenuItem onClick={handleSignIn}>
+					<ListItemIcon>
+						<Login fontSize="small" />
+					</ListItemIcon>
+					Sign In
+				</MenuItem>
+			)}
+		</Menu>
+	);
+
+	return (
+		<Box sx={{ flexGrow: 1 }}>
+			<Box sx={{ flexGrow: 1 }} />
+			<Box sx={{ display: { xs: "none", md: "flex" } }}>
+				{authCtx.isLoggedIn && (
+					<IconButton size="large" onClick={handleLibraryPage}>
+						<Chip
+							color="info"
+							icon={<LibraryBooksIcon />}
+							label="Library"
+						/>
+					</IconButton>
 				)}
-			</Menu>
-		</React.Fragment>
+				<IconButton size="large" onClick={handleHomePage}>
+					<Chip color="info" icon={<Home />} label="Home" />
+				</IconButton>
+				{!authCtx.isLoggedIn && (
+					<IconButton size="large" onClick={handleSignIn}>
+						<Chip color="info" icon={<Login />} label="Sign In" />
+					</IconButton>
+				)}
+				{authCtx.isLoggedIn && (
+					<Tooltip title="Account">
+						<IconButton
+							size="large"
+							edge="end"
+							aria-controls={menuId}
+							aria-haspopup="true"
+							onClick={handleProfileMenuOpen}
+						>
+							<Avatar />
+						</IconButton>
+					</Tooltip>
+				)}
+			</Box>
+			<Box sx={{ display: { xs: "flex", md: "none" } }}>
+				<IconButton
+					size="large"
+					aria-label="show more"
+					aria-controls={mobileMenuId}
+					aria-haspopup="true"
+					onClick={handleMobileMenuOpen}
+				>
+					<MoreIcon />
+				</IconButton>
+			</Box>
+			{renderMobileMenu}
+			{renderMenu}
+		</Box>
 	);
 };
 
